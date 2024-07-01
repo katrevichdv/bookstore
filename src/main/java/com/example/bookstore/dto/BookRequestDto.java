219137208da49d0d1b203da917e6892aabf92c88
Dmytro Katrevych <katrevichdv@gmail.com>
2024-07-01 11:55:41 +0300

    Added validation for existing DTO classes

--- src/main/java/com/example/bookstore/dto/BookRequestDto.java
+++ src/main/java/com/example/bookstore/dto/BookRequestDto.java
@@ -1,11 +1,18 @@
 package com.example.bookstore.dto;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public record BookRequestDto(
+        @NotBlank
         String title,
+        @NotBlank
         String author,
+        @NotBlank
         String isbn,
+        @Min(0) @Digits(integer = 8, fraction = 2)
         String price,
         String description,
         String coverImage) {
-
 }
--- src/main/java/com/example/bookstore/dto/BookSearchParametersDto.java
+++ src/main/java/com/example/bookstore/dto/BookSearchParametersDto.java
@@ -1,12 +1,16 @@
 package com.example.bookstore.dto;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
 import java.util.List;
 
 public record BookSearchParametersDto(
         List<String> title,
         List<String> author,
         List<String> isbn,
+        @Min(0) @Digits(integer = 8, fraction = 2)
         String minPrice,
+        @Min(0) @Digits(integer = 8, fraction = 2)
         String maxPrice,
         List<String> description) {
 }
