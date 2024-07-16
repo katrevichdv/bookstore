c8900004a2e530710cd6b894c14da4d8d8047ce1
Dmytro Katrevych <katrevichdv@gmail.com>
2024-07-16 15:00:40 +0300

    Length validation added

--- src/main/java/com/example/bookstore/dto/BookRequestDto.java
+++ src/main/java/com/example/bookstore/dto/BookRequestDto.java
@@ -3,16 +3,22 @@ package com.example.bookstore.dto;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public record BookRequestDto(
         @NotBlank
+        @Size(max = 255)
         String title,
         @NotBlank
+        @Size(max = 255)
         String author,
         @NotBlank
+        @Size(max = 255)
         String isbn,
         @Positive @Digits(integer = 8, fraction = 2)
         String price,
+        @Size(max = 512)
         String description,
+        @Size(max = 512)
         String coverImage) {
 }
