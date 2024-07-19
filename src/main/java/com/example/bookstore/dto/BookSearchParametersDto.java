70b06e35916cf56c6e7ef6baeba0ce93bc9a08f6
Dmytro Katrevych <katrevichdv@gmail.com>
2024-07-19 14:52:28 +0300

    Whitespaces removed

--- src/main/java/com/example/bookstore/dto/BookSearchParametersDto.java
+++ src/main/java/com/example/bookstore/dto/BookSearchParametersDto.java
@@ -6,12 +6,12 @@ import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record BookSearchParametersDto(
-        List<@Size(max = 255) String> title,
+        List<@Size(max = 255)String> title,
         List<@Size(max = 255)String> author,
         List<@Size(max = 255)String> isbn,
         @Positive @Digits(integer = 8, fraction = 2)
         String minPrice,
         @Positive @Digits(integer = 8, fraction = 2)
         String maxPrice,
-        List<@Size(max = 512) String> description) {
+        List<@Size(max = 512)String> description) {
 }
