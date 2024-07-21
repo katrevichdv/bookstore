e3861857695055b69affa4f470fca7f53ed50a1a
Dmytro Katrevych <katrevichdv@gmail.com>
2024-07-21 15:27:27 +0300

    UserRequestDto constraints added

--- src/main/java/com/example/bookstore/dto/user/UserRequestDto.java
+++ src/main/java/com/example/bookstore/dto/user/UserRequestDto.java
@@ -1,11 +1,25 @@
 package com.example.bookstore.dto.user;
 
+import com.example.bookstore.validation.constraints.FieldMatch;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@FieldMatch(field = "password", fieldMatch = "repeatPassword", message = "Passwords don't match")
 public record UserRequestDto(
+        @NotNull
+        @Email
         String email,
+        @NotBlank
+        @Size(min = 8)
         String password,
+        @NotBlank
+        @Size(min = 8)
         String repeatPassword,
+        @NotBlank
         String firstName,
+        @NotBlank
         String lastName,
-        String shippingAddress
-) {
+        String shippingAddress) {
 }
