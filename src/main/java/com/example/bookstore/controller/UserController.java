4de3dcad771459ae9468ba5aa3f3da2884092b7e
Dmytro Katrevych <katrevichdv@gmail.com>
2024-07-21 13:49:02 +0300

    UserController added

---
+++ src/main/java/com/example/bookstore/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.bookstore.controller;
+
+import com.example.bookstore.dto.user.UserRequestDto;
+import com.example.bookstore.dto.user.UserResponseDto;
+import com.example.bookstore.exception.RegistrationException;
+import com.example.bookstore.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/auth")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/registration")
+    public UserResponseDto registerUser(@RequestBody @Valid UserRequestDto requestDto)
+            throws RegistrationException {
+        return userService.register(requestDto);
+    }
+}
