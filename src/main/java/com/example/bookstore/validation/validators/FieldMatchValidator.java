8f6916324ed4f41269bf7e59593441df9a28f8eb
Dmytro Katrevych <katrevichdv@gmail.com>
2024-07-21 15:27:13 +0300

    FieldMatch constraint and validator added

---
+++ src/main/java/com/example/bookstore/validation/validators/FieldMatchValidator.java
@@ -0,0 +1,25 @@
+package com.example.bookstore.validation.validators;
+
+import com.example.bookstore.validation.constraints.FieldMatch;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.Objects;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object> {
+    private String field;
+    private String fieldMatch;
+
+    @Override
+    public void initialize(FieldMatch constraintAnnotation) {
+        this.field = constraintAnnotation.field();
+        this.fieldMatch = constraintAnnotation.fieldMatch();
+    }
+
+    @Override
+    public boolean isValid(Object o, ConstraintValidatorContext constraintValidatorContext) {
+        Object fieldValue = new BeanWrapperImpl(o).getPropertyValue(field);
+        Object fieldMatchValue = new BeanWrapperImpl(o).getPropertyValue(fieldMatch);
+        return Objects.equals(fieldValue, fieldMatchValue);
+    }
+}
