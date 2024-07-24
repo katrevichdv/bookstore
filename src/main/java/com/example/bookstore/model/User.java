f0b36af3719ef8f4e828ecb22e59b870121975ad
Dmytro Katrevych <katrevichdv@gmail.com>
2024-07-24 17:37:09 +0300

    Entities annotations changed

--- src/main/java/com/example/bookstore/model/User.java
+++ src/main/java/com/example/bookstore/model/User.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity(name = "users")
 public class User {
     @Id
