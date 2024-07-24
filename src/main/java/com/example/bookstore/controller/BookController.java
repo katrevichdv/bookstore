170d923999989c8d3259ff0372c0fd703f5f9f21
Dmytro Katrevych <katrevichdv@gmail.com>
2024-07-24 16:26:37 +0300

    servlet.context-path property added

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/books")
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
