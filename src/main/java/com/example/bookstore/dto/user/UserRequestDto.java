67087a3fde6e198e206bb14934bd0e2d6d843ae6
Dmytro Katrevych <katrevichdv@gmail.com>
2024-07-24 17:34:23 +0300

    Email field annotation changed to NotBlank

--- src/main/java/com/example/bookstore/dto/user/UserRequestDto.java
+++ src/main/java/com/example/bookstore/dto/user/UserRequestDto.java
@@ -3,13 +3,12 @@ package com.example.bookstore.dto.user;
 import com.example.bookstore.validation.constraints.FieldMatch;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @FieldMatch(field = "password", fieldMatch = "repeatPassword", message = "Passwords don't match")
 public record UserRequestDto(
-        @NotNull
         @Email
+        @NotBlank
         String email,
         @NotBlank
         @Size(min = 8)
