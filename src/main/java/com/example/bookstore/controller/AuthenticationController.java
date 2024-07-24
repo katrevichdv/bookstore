6435ead9297c85f20b5e31b03ba35e29134061e9
Dmytro Katrevych <katrevichdv@gmail.com>
2024-07-24 17:53:16 +0300

    Checkstyle issue fixed

--- src/main/java/com/example/bookstore/controller/AuthenticationController.java
+++ src/main/java/com/example/bookstore/controller/AuthenticationController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
     private final UserService userService;
+
     @PostMapping("/registration")
     public UserResponseDto registerUser(@RequestBody @Valid UserRequestDto requestDto)
             throws RegistrationException {
