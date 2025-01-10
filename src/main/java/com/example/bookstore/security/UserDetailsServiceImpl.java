6390fd86a17bce24ccb3e0072f28e4fc02d9bf9d
Katrevych <katrevichdv@gmail.com>
2025-01-10 23:15:48 +0200

    SecurityConfig, UserDetailsServiceImpl were added

---
+++ src/main/java/com/example/bookstore/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.bookstore.security;
+
+import com.example.bookstore.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Can't find User by email: " + username));
+    }
+}
